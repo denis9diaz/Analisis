80d0feacfa95a204580270d822c0f6255e1e315e
Denis <denis9diaz@hotmail.com>
2025-03-28 11:46:49 +0100

    instalado astro y tailwind y creada estructura

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,ts,jsx,tsx}", // Ajusta las rutas según tu proyecto
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
