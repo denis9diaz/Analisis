41803eae7e1db411447be50dd1326989a60c34d5
Denis <denis9diaz@hotmail.com>
2025-04-05 14:23:12 +0200

    modificada logica de token y refresh

--- backend/auth/urls.py
+++ backend/auth/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('register/', views.register, name="register"),
     path('login/', views.login, name="login"),
     path('logout/', views.logout, name='logout'),
     path('delete-user/', views.delete_user, name='delete_user'),
+
+    # JWT endpoints estándar
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
