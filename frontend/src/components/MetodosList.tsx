81fd622476757335af135e53a2570380967d6c8c
Denis <denis9diaz@hotmail.com>
2025-04-06 12:58:59 +0200

    creado boton añadir metodo y archivo modal de metodo

--- frontend/src/components/MetodosList.tsx
+++ frontend/src/components/MetodosList.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useMetodo } from "../context/MetodoContext";
 import { fetchWithAuth } from "../utils/authFetch";
+import MetodoFormModal from "./MetodoFormModal";
 
 type Metodo = {
   id: number;
@@ -10,6 +11,7 @@ type Metodo = {
 export default function MetodosList() {
   const [metodos, setMetodos] = useState<Metodo[]>([]);
   const { metodoSeleccionado, setMetodoSeleccionado } = useMetodo();
+  const [showModalMetodo, setShowModalMetodo] = useState(false);
 
   useEffect(() => {
     fetchWithAuth("http://localhost:8000/api/general/metodos/")
@@ -28,6 +30,12 @@ export default function MetodosList() {
   return (
     <div className="mb-4">
       <h3 className="text-xl font-semibold mb-4 text-gray-700">Mis Métodos</h3>
+      <button
+        onClick={() => setShowModalMetodo(true)}
+        className="mb-4 bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-6 rounded-lg shadow transition"
+      >
+        Añadir Método
+      </button>
       <ul className="space-y-2">
         {metodos.map((metodo) => {
           const isSelected = metodoSeleccionado?.id === metodo.id;
@@ -47,6 +55,16 @@ export default function MetodosList() {
           );
         })}
       </ul>
+
+      <MetodoFormModal
+      isOpen={showModalMetodo}
+      onRequestClose={() => setShowModalMetodo(false)}
+      onMetodoGuardado={() => {
+        fetchWithAuth("http://localhost:8000/api/general/metodos/")
+          .then((res) => res.json())
+          .then((data) => setMetodos(data));
+      }}
+    />
     </div>
   );
 }
